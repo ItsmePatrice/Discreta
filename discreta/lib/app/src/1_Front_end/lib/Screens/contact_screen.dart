1378cef5e127fe952c0bd496a4dc22774dc8d072
patriceammah <patriceammah@gmail.com>
2025-12-27 14:12:08 -0500

    save button added

--- discreta/lib/app/src/1_Front_end/lib/Screens/contact_screen.dart
+++ discreta/lib/app/src/1_Front_end/lib/Screens/contact_screen.dart
@@ -1,12 +1,13 @@
+import 'package:discreta/app/src/1_Front_end/Assets/enum/button_size.dart';
 import 'package:discreta/app/src/1_Front_end/Assets/enum/text_size.dart';
 import 'package:discreta/app/src/1_Front_end/lib/Classes/contact.dart';
 import 'package:discreta/app/src/1_Front_end/lib/Components/discreta_button.dart';
 import 'package:discreta/app/src/1_Front_end/lib/Components/discreta_text.dart';
 import 'package:discreta/app/src/1_Front_end/lib/Components/loading_overlay.dart';
 import 'package:discreta/app/src/1_Front_end/Assets/colors.dart';
-import 'package:discreta/app/src/1_Front_end/lib/Components/Layout/discreta_nav_bar.dart';
 import 'package:discreta/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ContactsPage extends StatefulWidget {
   const ContactsPage({super.key});
@@ -176,6 +177,10 @@ class _ContactsPageState extends State<ContactsPage> {
     });
   }
 
+  void _dismissKeyboard() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -194,82 +199,102 @@ class _ContactsPageState extends State<ContactsPage> {
       ),
       body: Stack(
         children: [
-          SingleChildScrollView(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                DiscretaText(
-                  text: AppLocalizations.of(context)!.alertMessage,
-                  fontWeight: FontWeight.bold,
-                  size: TextSize.medium,
-                ),
-                const SizedBox(height: 8),
-                TextField(
-                  controller: _alertController,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    hintText: AppLocalizations.of(
-                      context,
-                    )!.alertMessagePlaceholder,
+          GestureDetector(
+            onTap: _dismissKeyboard,
+            behavior: HitTestBehavior.translucent,
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DiscretaText(
+                    text: AppLocalizations.of(context)!.alertMessage,
+                    fontWeight: FontWeight.bold,
+                    size: TextSize.medium,
                   ),
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    DiscretaText(
-                      text: AppLocalizations.of(context)!.contacts,
-                      fontWeight: FontWeight.bold,
-                      size: TextSize.medium,
-                    ),
-                    DiscretaButton(
-                      onPressed: _addContact,
-                      text: AppLocalizations.of(context)!.addContact,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 12),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: _contacts.length,
-                  itemBuilder: (context, index) {
-                    final contact = _contacts[index];
-                    return Card(
-                      margin: const EdgeInsets.symmetric(vertical: 6),
-                      child: ListTile(
-                        title: Text(contact.name),
-                        subtitle: Text(contact.phone),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              icon: const Icon(
-                                Icons.edit,
-                                color: AppColors.primaryColor,
-                              ),
-                              onPressed: () => _editContact(index),
-                            ),
-                            IconButton(
-                              icon: const Icon(
-                                Icons.delete,
-                                color: AppColors.red,
-                              ),
-                              onPressed: () => _deleteContact(index),
-                            ),
-                          ],
-                        ),
+                  SizedBox(height: 8.h),
+                  TextField(
+                    controller: _alertController,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                    );
-                  },
-                ),
-              ],
+                      hintText: AppLocalizations.of(
+                        context,
+                      )!.alertMessagePlaceholder,
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DiscretaButton(
+                        text: AppLocalizations.of(context)!.save,
+                        size: ButtonSize.medium,
+                        onPressed: () {
+                          // Save alert message logic here
+                          _dismissKeyboard();
+                        },
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 70.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      DiscretaText(
+                        text: AppLocalizations.of(context)!.contacts,
+                        fontWeight: FontWeight.bold,
+                        size: TextSize.medium,
+                      ),
+                      DiscretaButton(
+                        onPressed: _addContact,
+                        text: AppLocalizations.of(context)!.addContact,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: _contacts.length,
+                    itemBuilder: (context, index) {
+                      final contact = _contacts[index];
+                      return Card(
+                        margin: const EdgeInsets.symmetric(vertical: 6),
+                        child: ListTile(
+                          title: Text(contact.name),
+                          subtitle: Text(contact.phone),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: AppColors.primaryColor,
+                                ),
+                                onPressed: () => _editContact(index),
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: AppColors.red,
+                                ),
+                                onPressed: () => _deleteContact(index),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
+
           if (_isLoading) const LoadingOverlay(),
         ],
       ),
