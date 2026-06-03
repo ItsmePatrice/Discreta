afcd7643116fae4067e89c133f151c1e5405a190
patriceammah <patriceammah@gmail.com>
2026-06-03 16:00:34 -0400

    logging added for error

--- discreta/lib/app/src/1_Front_end/lib/Services/http_service.dart
+++ discreta/lib/app/src/1_Front_end/lib/Services/http_service.dart
@@ -27,7 +27,7 @@ class HttpService {
       );
       return response;
     } catch (e) {
-      throw Exception('GET request failed: $e');
+      throw Exception('GET request failed while calling $path: $e');
     }
   }
 
