67b528599a9e50803660cfa41e56ae72ef759c98
patriceammah <patriceammah@gmail.com>
2026-06-21 20:11:05 -0400

    Notification service used for signout

--- discreta/lib/app/src/1_Front_end/lib/Services/auth_service.dart
+++ discreta/lib/app/src/1_Front_end/lib/Services/auth_service.dart
@@ -161,7 +161,7 @@ class AuthService {
 
   Future<void> signOutUser() async {
     try {
-      // await NotificationService.instance.signOut();
+      await NotificationService.instance.signOut();
       discretaUser = null;
       const secureStorage = FlutterSecureStorage(
         iOptions: IOSOptions(
